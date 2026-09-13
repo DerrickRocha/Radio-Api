899b6c5ab5d6247e2919115b7ded123f8d2eac51
derrick <drocha616@gmail.com>
2026-09-12 22:38:48 -0600

    Changed to correct service name.

--- RadioApi/publish.sh
+++ RadioApi/publish.sh
@@ -6,5 +6,5 @@ git pull
 dotnet publish -c Release -o /tmp/radio-publish
 sudo rsync -a --delete /tmp/radio-publish/ /opt/radio-api/
 sudo chown -R radioapi:radioapi /opt/radio-api
-sudo systemctl restart dotnet-radio-api
-sudo systemctl status dotnet-radio-api --no-pager
+sudo systemctl restart radio-api.service
+sudo systemctl status radio-api.service --no-pager
